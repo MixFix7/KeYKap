5f45f98b30a64271e22c1313e31da3d407cf83ce
MixFix7 <mashtalar95@gmail.com>
2023-06-26 19:09:15 +0300

    update ProductPage.jsx

--- frontend/src/components/ProductPage/ProductPage.jsx
+++ frontend/src/components/ProductPage/ProductPage.jsx
@@ -14,28 +14,35 @@ const ProductPage = () => {
     <div className='container mx-auto p-4'>
       <div className='Images flex flex-wrap'>
         <div className='w-full md:w-1/2'>
+          <div className='flex flex-col h-full'>
             {product.map((image, index) => 
               <ProductImages key={index} image={image.specs.image}/>      
             )}
-        
-          <div className='flex justify-center space-x-2'>
-            <button className='border border-gray-300 w-1/4 py-2 rounded'>
-              <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
-            </button>
-            <button className='border border-gray-300 w-1/4 py-2 rounded'>
-              <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
-            </button>
-            <button className='border border-gray-300 w-1/4 py-2 rounded'>
-              <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
-            </button>
-            <button className='border border-gray-300 w-1/4 py-2 rounded'>
-              <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
-            </button>
+            <div className='flex justify-center space-x-2'>
+              <button className='border border-gray-300 w-1/4 py-2 rounded'>
+                <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
+              </button>
+              <button className='border border-gray-300 w-1/4 py-2 rounded'>
+                <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
+              </button>
+              <button className='border border-gray-300 w-1/4 py-2 rounded'>
+                <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
+              </button>
+              <button className='border border-gray-300 w-1/4 py-2 rounded'>
+                <img src="http://127.0.0.1:8000//media/image_products/maxresdefault.jpg" alt="image_1" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className='Label flex justify-start'>
-          <h1 className='text-white text-left'>{data.name}</h1>
+      <div className='w-full md:w-1/2'>
+        <div className='flex flex-col h-full justify-center'>
+          <h1 className='text-white text-2xl font-bold mb-2'>{data.name}</h1>
+          <p className='text-gray-200 font-bold mb-2'>{data.specs.description}</p>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+              Add to cart
+          </button>
+        </div>   
       </div>
     </div>
   );
