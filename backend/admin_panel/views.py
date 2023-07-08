16ad57ecbe6ecb03baacbe885648ec9d6fc554af
MixFix7 <mashtalar95@gmail.com>
2023-07-08 12:51:33 +0300

    add fields to views.py and AddProductPage.jsx

--- backend/admin_panel/views.py
+++ backend/admin_panel/views.py
@@ -10,19 +10,41 @@ class AddProduct(APIView):
         category = request.data.get('category')
         product_name = request.data.get('product_name')
         price = request.data.get('price')
-        print(category, product_name, price)
-        # product = request.data.get('product')
-        # brand = request.data.get('brand')
-        # description = request.data.get('description')
-        # color = request.data.get('color')
-        # keyboard_type = request.data.get('keyboard_type')
-        # switches_name = request.data.get('switches_name')
-        # switches_img = request.data.get('switches_img')
-        # lubricated = request.data.get('lubricated')
+        brand = request.data.get('brand')
+        description = request.data.get('description')
+        color = request.data.get('color')
+        keyboard_type = request.data.get('keyboard_type')
+        switches_name = request.data.get('switches_name')
+        switches_img = request.FILES.get('switches_img')
+        lubricated = request.data.get('lubricated')
         # photos = request.FILES.get('photos')
 
-        category_queryset = Category.objects.get(category=category)
-        Product.objects.create(category=category_queryset, name=product_name, price=price)
+        print(brand,
+            description,
+            color,
+            keyboard_type,
+            switches_name,
+            switches_img,
+            lubricated,
+              price,
+        )
+
+        product = Product.objects.create(
+            category=Category.objects.get(category=category),
+            name=product_name,
+            price=price
+        )
+
+        keyboard_specs = KeyboardSpecs.objects.create(
+            product=product,
+            brand=brand,
+            description=description,
+            color=color,
+            keyboard_type=keyboard_type,
+            switches_name=switches_name,
+            switches_img=switches_img,
+            lubricated=bool(lubricated),
+        )
 
         return Response(status=status.HTTP_201_CREATED)
 
--- frontend/src/components/Admin_Panel/AddProductPage/AddProductPage.jsx
+++ frontend/src/components/Admin_Panel/AddProductPage/AddProductPage.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CountInCart from './../../Home/UI/CountInCart';
+import axios from 'axios';
 
 const AddProductPage = () => {
+  const [categories, SetCategories] = useState([]) 
 
     const addProductonApi = async (formData) => {
         const response = await fetch('http://localhost:8000/admin-panel/add-product/', {
@@ -20,18 +22,58 @@ const AddProductPage = () => {
         formData.append('category', e.target.category.value)
         formData.append('product_name', e.target.name.value)
         formData.append('price', e.target.price.value)
+        formData.append('brand', e.target.brand.value)
+        formData.append('description', e.target.description.value)
+        formData.append('keyboard_type', e.target.keyboard_type.value)
+        formData.append('switches_name', e.target.switches_name.value)
+        formData.append('switches_img', e.target.switches_img.files[0])
+        formData.append('lubricated', e.target.lubricated.value)
 
         addProductonApi(formData)
-
     }
+
+    const getCategories = async () => {
+      const response = await axios.get('http://localhost:8000/api/allCategories/')
+
+        SetCategories(response.data)
+        console.log(categories)
+      
+    }
+
+    useEffect(() => {
+      getCategories()
+    }, [])
+    
+
 
   return (
     <div className=''>
-      <h1>Add Product</h1>
+      <h1 className='text-white'>Add Product</h1>
       <form onSubmit={handleFormAddProductToApi}>
-        <input type="text" name='category'/>
+        <h1 className='text-white'>Category</h1>
+        <select name="category">
+          {categories.map((category) => (
+            <option key={category.id} value={category.category}>{category.category}</option>
+          ))}
+        </select>
+        <h1 className='text-white'>name</h1>
         <input type="text" name="name"/>
-        <input type="number" name="price"/>
+        <h1 className='text-white'>price</h1>
+        <input type="text" name="price"/>
+        <h1 className='text-white'>brand</h1>
+        <input type="text" name="brand"/>
+        <h1 className='text-white'>description</h1>
+        <textarea type="text" name="description"/>
+        <h1 className='text-white'>color</h1>
+        <input type="text" name="color"/>
+        <h1 className='text-white'>keyboard_type</h1>
+        <input type="text" name="keyboard_type"/>
+        <h1 className='text-white'>switches_name</h1>
+        <input type="text" name="switches_name"/>
+        <h1 className='text-white'>switches_img</h1>
+        <input type="file" name="switches_img"/>
+        <h1 className='text-white'>lubricated</h1>
+        <input type="text" name="lubricated"/>
         <button className='text-white' type='submit'>add</button>
       </form>
     </div>
